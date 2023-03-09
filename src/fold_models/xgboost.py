eeb4cf7d7e20372ed3b64473504a0b84dfe4a7d2
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-09 12:37:20 +0100

    fix(API): updated to follow new fold Model API

--- src/fold_models/xgboost.py
+++ src/fold_models/xgboost.py
@@ -1,19 +1,17 @@
-from typing import Any, Optional
+from typing import Any, Optional, Union
 
 import pandas as pd
 from fold.models.base import Model
 
 
-class XGB(Model):
+class WrapXGB(Model):
     properties = Model.Properties()
-
-    fitted = False
 
     def __init__(self, model: Any) -> None:
         from xgboost import XGBClassifier, XGBRegressor, XGBRFClassifier, XGBRFRegressor
 
         self.model = model
-        self.name = "XGB-{model.__class__.__name__}"
+        self.name = f"XGB-{model.__class__.__name__}"
         if isinstance(self.model, XGBRegressor) or isinstance(
             self.model, XGBRFRegressor
         ):
@@ -28,16 +26,20 @@ class XGB(Model):
     def fit(
         self, X: pd.DataFrame, y: pd.Series, sample_weights: Optional[pd.Series] = None
     ) -> None:
-        if self.fitted:
-            self.model.fit(
-                X,
-                y,
-                sample_weight=sample_weights,
-                xgb_model=self.model.get_booster(),
-            )
-        else:
-            self.model.fit(X, y, sample_weight=sample_weights)
-            self.fitted = True
+        self.model.fit(X, y, sample_weight=sample_weights)
 
-    def predict(self, X: pd.DataFrame) -> pd.DataFrame:
+    def update(
+        self, X: pd.DataFrame, y: pd.Series, sample_weights: Optional[pd.Series] = None
+    ) -> None:
+        self.model.fit(
+            X,
+            y,
+            sample_weight=sample_weights,
+            xgb_model=self.model.get_booster(),
+        )
+
+    def predict(self, X: pd.DataFrame) -> Union[pd.Series, pd.DataFrame]:
+        return pd.Series(self.model.predict(X), index=X.index)
+
+    def predict_in_sample(self, X: pd.DataFrame) -> Union[pd.Series, pd.DataFrame]:
         return pd.Series(self.model.predict(X), index=X.index)
--- tests/test_statsforecast.py
+++ tests/test_statsforecast.py
@@ -1,20 +1,38 @@
 from fold.loop import backtest, train
 from fold.splitters import ExpandingWindowSplitter
-from fold.utils.tests import generate_zeros_and_ones_skewed
-from statsforecast.models import Naive
+from fold.utils.tests import generate_sine_wave_data
+from statsforecast.models import ARIMA, Naive
 
-from fold_models.statsforecast import UnivariateStatsForecast
+from fold_models.statsforecast import WrapStatsForecast
 
 
-def test_statsforecast_univariate() -> None:
-    X = generate_zeros_and_ones_skewed(70, weights=[0.5, 0.5])
+def test_statsforecast_univariate_naive() -> None:
+    X = generate_sine_wave_data(resolution=70)
     y = X.shift(-1).squeeze()
 
-    splitter = ExpandingWindowSplitter(train_window_size=50, step=10)
-    transformations = UnivariateStatsForecast(Naive())
+    splitter = ExpandingWindowSplitter(train_window_size=50, step=1)
+
+    transformations = WrapStatsForecast(
+        model_class=Naive, init_args={}, use_exogenous=False
+    )
 
     transformations_over_time = train(transformations, X, y, splitter)
-    _, pred = backtest(transformations_over_time, X, y, splitter)
+    pred = backtest(transformations_over_time, X, y, splitter)
     assert (
         y.squeeze()[pred.index][:-1] == pred.shift(-1).squeeze()[:-1].astype(int)
     ).all()
+
+
+def test_statsforecast_univariate_arima() -> None:
+    X = generate_sine_wave_data(resolution=70)
+    y = X.shift(-1).squeeze()
+
+    splitter = ExpandingWindowSplitter(train_window_size=50, step=1)
+
+    transformations = transformations = WrapStatsForecast(
+        model_class=ARIMA, init_args={"order": (1, 0, 0)}, use_exogenous=False
+    )
+
+    transformations_over_time = train(transformations, X, y, splitter)
+    pred = backtest(transformations_over_time, X, y, splitter)
+    assert (y.squeeze()[pred.index][:-1] == pred.squeeze()[:-1].astype(int)).all()
