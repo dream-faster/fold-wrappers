1c9c8a6280ea23d766a227a3c51f33c20533d891
Daniel Szemerey <account@szemerey.eu>
2023-03-14 16:40:53 +0100

    feature(Models): Added wrapper for ``sktime`` (https://github.com/sktime/sktime)

---
+++ tests/test_sktime.py
@@ -0,0 +1,14 @@
+from fold.splitters import ExpandingWindowSplitter
+from sktime.forecasting.naive import NaiveForecaster
+from utils import run_pipeline_and_check_if_results_are_close
+
+from fold_models.sktime import WrapSktime
+
+
+def test_statsforecast_univariate_autoarima() -> None:
+    run_pipeline_and_check_if_results_are_close(
+        model=WrapSktime(
+            model_class=NaiveForecaster, init_args={}, use_exogenous=False
+        ),
+        splitter=ExpandingWindowSplitter(initial_train_window=50, step=1),
+    )
