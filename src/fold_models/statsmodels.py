35da459f65a0acfe2129882e58a9a895ad0d5f3f
Daniel Szemerey <account@szemerey.eu>
2023-03-15 11:24:12 +0100

    feature(Models): Added test and initial wrapper for ``statsmodels`` library (https://github.com/statsmodels/statsmodels). (#23)
    * feature(Wrapper): Added test and initial wrapper for ``statsmodels`` library (https://www.statsmodels.org/).
    
    * feature: Implementing the ``res`` pattern for ``statsmodels``
    
    * feature: Univariate statsmodel now passes.
    
    * feature: Added workflow
    
    * feature: Updated pyproject optional dependencies.
    
    * feature(Model): Multivariate models work now!
    
    * feature: New test for live updating models.
    
    * feature: Added multivariate and univariate live model.
    
    * feature: Added precommit
    
    * fix: Import errors detected by flake
    
    * docs: Updated readme to include statsmodels.
    
    * fix(StatsModel): wrapper support online mode now
    
    * fix(StatsForecast): tests are working
    
    ---------
    
    Co-authored-by: Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>

---
+++ src/fold_models/statsmodels.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+from typing import Any, Optional, Type, Union
+
+import pandas as pd
+from fold.models.base import Model
+
+
+class WrapStatsModels(Model):
+    properties = Model.Properties(
+        model_type=Model.Properties.ModelType.regressor,
+    )
+
+    def __init__(
+        self,
+        model_class: Type,
+        init_args: dict,
+        use_exogenous: bool,
+        online_mode: bool = False,
+        instance: Optional[Any] = None,
+    ) -> None:
+        self.model_class = model_class
+        self.init_args = init_args
+        self.use_exogenous = use_exogenous
+        self.properties.mode = (
+            Model.Properties.Mode.online
+            if online_mode
+            else Model.Properties.Mode.minibatch
+        )
+        self.name = f"WrapStatsModels-{self.model_class.__class__.__name__}"
+        self.instance = instance
+
+    @classmethod
+    def from_model(
+        cls,
+        model,
+        use_exogenous: bool,
+        online_mode: bool = False,
+    ) -> WrapStatsModels:
+        return cls(
+            model_class=None,
+            init_args=None,
+            use_exogenous=use_exogenous,
+            instance=model,
+            online_mode=online_mode,
+        )
+
+    def fit(
+        self, X: pd.DataFrame, y: pd.Series, sample_weights: Optional[pd.Series] = None
+    ) -> None:
+        if self.use_exogenous:
+            self.model = (
+                self.model_class(y, X, **self.init_args)
+                if self.instance is None
+                else self.instance
+            )
+            self.model = self.model.fit()
+        else:
+            self.model = (
+                self.model_class(y, **self.init_args)
+                if self.instance is None
+                else self.instance
+            )
+            self.model = self.model.fit()
+
+    def update(
+        self, X: pd.DataFrame, y: pd.Series, sample_weights: Optional[pd.Series] = None
+    ) -> None:
+        if not hasattr(self.model, "append"):
+            return
+
+        if self.use_exogenous:
+            self.model = self.model.append(endog=y, exog=X, refit=True)
+        else:
+            self.model = self.model.append(endog=y, refit=True)
+
+    def predict(self, X: pd.DataFrame) -> Union[pd.Series, pd.DataFrame]:
+        if self.use_exogenous:
+            return pd.Series(
+                self.model.predict(start=X.index[0], end=X.index[-1], exog=X)
+            )
+        else:
+            return pd.Series(self.model.predict(start=X.index[0], end=X.index[-1]))
+
+    def predict_in_sample(self, X: pd.DataFrame) -> Union[pd.Series, pd.DataFrame]:
+        return self.model.predict(start=X.index[0], end=X.index[-1])
---
+++ tests/test_statsmodels.py
@@ -0,0 +1,59 @@
+from fold.splitters import ExpandingWindowSplitter
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from utils import run_pipeline_and_check_if_results_are_close
+
+from fold_models.statsmodels import WrapStatsModels
+
+
+def test_statsmodels_univariate_arima() -> None:
+    run_pipeline_and_check_if_results_are_close(
+        model=WrapStatsModels(
+            model_class=ARIMA, init_args={"order": (1, 0, 0)}, use_exogenous=False
+        ),
+        splitter=ExpandingWindowSplitter(initial_train_window=50, step=1),
+    )
+
+
+def test_statsforecast_univariate_arima_online() -> None:
+    run_pipeline_and_check_if_results_are_close(
+        model=WrapStatsModels(
+            model_class=ARIMA,
+            init_args={"order": (1, 0, 0)},
+            use_exogenous=False,
+            online_mode=True,
+        ),
+        splitter=ExpandingWindowSplitter(initial_train_window=50, step=10),
+    )
+
+
+def test_statsforecast_multivariate_arima_online() -> None:
+    run_pipeline_and_check_if_results_are_close(
+        model=WrapStatsModels(
+            model_class=ARIMA,
+            init_args={"order": (1, 0, 0)},
+            use_exogenous=True,
+            online_mode=True,
+        ),
+        splitter=ExpandingWindowSplitter(initial_train_window=50, step=10),
+    )
+
+
+def test_statsmodels_multivariate_arima() -> None:
+    run_pipeline_and_check_if_results_are_close(
+        model=WrapStatsModels(
+            model_class=ARIMA, init_args={"order": (1, 0, 0)}, use_exogenous=True
+        ),
+        splitter=ExpandingWindowSplitter(initial_train_window=50, step=1),
+    )
+
+
+def test_statsmodels_univariate_exponential_smoothing() -> None:
+    run_pipeline_and_check_if_results_are_close(
+        model=WrapStatsModels(
+            model_class=ExponentialSmoothing,
+            init_args={},
+            use_exogenous=False,
+        ),
+        splitter=ExpandingWindowSplitter(initial_train_window=50, step=1),
+    )
