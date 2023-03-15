5fb63c48df744ba4ee0e0179e99d509b334d323a
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-15 15:09:54 +0100

    fix(XGBoost): upgraded to use the new initialization convention (#28)

--- tests/test_xgboost.py
+++ tests/test_xgboost.py
@@ -10,7 +10,18 @@ def test_xgboost() -> None:
     X, y = generate_sine_wave_data()
 
     splitter = ExpandingWindowSplitter(initial_train_window=500, step=100)
-    transformations = WrapXGB(XGBRegressor())
+    transformations = WrapXGB.from_model(XGBRegressor())
+
+    transformations_over_time = train(transformations, X, y, splitter)
+    pred = backtest(transformations_over_time, X, y, splitter)
+    assert (y.squeeze()[pred.index] - pred.squeeze()).abs().sum() < 20
+
+
+def test_xgboost_init_with_args() -> None:
+    X, y = generate_sine_wave_data()
+
+    splitter = ExpandingWindowSplitter(initial_train_window=500, step=100)
+    transformations = WrapXGB(XGBRegressor, {"n_estimators": 100})
 
     transformations_over_time = train(transformations, X, y, splitter)
     pred = backtest(transformations_over_time, X, y, splitter)
