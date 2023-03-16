2d054e7a14393737e0dac0ddf5bf1d896595748e
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-16 17:38:44 +0100

    feature(Tests): added RNN tests

--- tests/test_neuralforecast.py
+++ tests/test_neuralforecast.py
@@ -2,7 +2,7 @@ import numpy as np
 from fold.loop import backtest, train
 from fold.splitters import ExpandingWindowSplitter
 from fold.utils.tests import generate_monotonous_data
-from neuralforecast.models import NBEATS, NHITS
+from neuralforecast.models import NBEATS, NHITS, RNN
 
 from fold_models.neuralforecast import WrapNeuralForecast
 
@@ -36,6 +36,25 @@ def test_neuralforecast_nhits() -> None:
 
     transformations = WrapNeuralForecast.from_model(
         NHITS(
+            input_size=input_size,
+            h=step,
+            max_epochs=50,
+        ),
+    )
+    transformations_over_time = train(transformations, X, y, splitter)
+    pred = backtest(transformations_over_time, X, y, splitter)
+    assert np.isclose(y.squeeze()[pred.index], pred.squeeze(), atol=0.2).all()
+
+
+def test_neuralforecast_rnn() -> None:
+    X, y = generate_monotonous_data()
+
+    step = 100
+    input_size = 100
+    splitter = ExpandingWindowSplitter(initial_train_window=400, step=step)
+
+    transformations = WrapNeuralForecast.from_model(
+        RNN(
             input_size=input_size,
             h=step,
             max_epochs=50,
