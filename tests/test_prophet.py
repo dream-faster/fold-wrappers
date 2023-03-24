4d40bdc10637fa6f32055f05ce54547ba719dea7
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-24 09:56:41 +0000

    fix(Tests): generate data with daily seasonality

--- tests/test_prophet.py
+++ tests/test_prophet.py
@@ -2,19 +2,19 @@ from fold.loop import backtest, train
 from fold.splitters import ExpandingWindowSplitter
 from fold.utils.tests import generate_sine_wave_data
 from prophet import Prophet
-
+import numpy as np
 from fold_models.prophet import WrapProphet
 
 
 def test_prophet() -> None:
-    X, y = generate_sine_wave_data(cycles=100, length=1200, freq="D")
+    X, y = generate_sine_wave_data(cycles=100, length=2400, freq="H")
 
-    splitter = ExpandingWindowSplitter(initial_train_window=0.5, step=0.05)
+    splitter = ExpandingWindowSplitter(initial_train_window=0.5, step=0.1)
     transformations = WrapProphet.from_model(Prophet())
 
     transformations_over_time = train(transformations, X, y, splitter)
     pred = backtest(transformations_over_time, X, y, splitter)
-    assert (y.squeeze()[pred.index][:-1] - pred.squeeze()[:-1]).abs().sum() < 20
+    assert np.isclose(y.squeeze()[pred.index], pred.squeeze(), atol=0.1).all()
 
 
 test_prophet()
