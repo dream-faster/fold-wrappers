5fff2b2d3b121c3c9c16d783f92a478933284445
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-04-12 14:28:52 +0200

    feature(Tests): added tests for automatic wrapping of 3rd party models

--- tests/test_statsforecast.py
+++ tests/test_statsforecast.py
@@ -1,6 +1,6 @@
 import numpy as np
-from fold.loop import backtest, train
-from fold.splitters import ExpandingWindowSplitter
+from fold.loop import train_backtest
+from fold.splitters import ExpandingWindowSplitter, SingleWindowSplitter
 from fold.utils.tests import generate_monotonous_data
 from statsforecast.models import ARIMA, MSTL, AutoARIMA, Naive
 from utils import (
@@ -16,8 +16,7 @@ def test_statsforecast_univariate_naive() -> None:
 
     splitter = ExpandingWindowSplitter(initial_train_window=50, step=1)
     pipeline = WrapStatsForecast(model_class=Naive, init_args={})
-    trained_pipelines = train(pipeline, None, y, splitter)
-    pred = backtest(trained_pipelines, None, y, splitter)
+    pred = train_backtest(pipeline, None, y, splitter)
     assert np.isclose(
         y.squeeze().shift(1)[pred.index][:-1], pred.squeeze()[:-1], atol=0.01
     ).all()
@@ -40,7 +39,17 @@ def test_statsforecast_exogenous_autoarima() -> None:
 def test_statsforecast_univariate_arima() -> None:
     run_pipeline_and_check_if_results_close_univariate(
         model=WrapStatsForecast(model_class=ARIMA, init_args={"order": (1, 0, 0)}),
-        splitter=ExpandingWindowSplitter(initial_train_window=50, step=2),
+        splitter=ExpandingWindowSplitter(initial_train_window=50, step=5),
+    )
+
+
+def test_automatic_wrapping_statsforecast() -> None:
+    _, y = generate_monotonous_data()
+    train_backtest(
+        ARIMA(order=(1, 1, 0)),
+        None,
+        y,
+        splitter=SingleWindowSplitter(0.5),
     )
 
 
