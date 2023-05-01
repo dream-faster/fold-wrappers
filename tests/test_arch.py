775dcf2f89ec922fb68949cb6558fa2d94b745e0
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-05-01 14:21:47 +0200

    fix(Tests): make it faster

--- tests/test_arch.py
+++ tests/test_arch.py
@@ -10,6 +10,6 @@ def test_arch_univariate() -> None:
     _, y = generate_sine_wave_data(length=200)
     y = np.log(y + 2.0).diff().dropna() * 100
     model = WrapArch(init_args=dict(vol="Garch", p=1, o=1, q=1, dist="Normal"))
-    splitter = ExpandingWindowSplitter(initial_train_window=0.5, step=1)
+    splitter = ExpandingWindowSplitter(initial_train_window=0.5, step=0.1)
     pred, _ = train_backtest(model, None, y, splitter)
     # assert np.isclose(y[pred.index] ** 2, pred.squeeze().values, atol=0.1).all()
