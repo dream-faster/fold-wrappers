1cb55e132487fc4137a4c0a37455d1488fe4690f
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-05-01 14:19:58 +0200

    fix(Arch): finished integration

--- src/fold_wrappers/arch.py
+++ src/fold_wrappers/arch.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Any, Optional, Union
 
 import pandas as pd
+from fold.base import fit_noop
 from fold.models.base import Model
 from fold.utils.checks import is_X_available
 
@@ -41,33 +42,22 @@ class WrapArch(Model):
         )
         if use_exogenous:
             self.model = arch_model(y, x=X, **self.init_args)
-            self.model = self.model.fit()
         else:
             self.model = arch_model(y, **self.init_args)
-            self.model = self.model.fit()
-
-    def update(
-        self, X: pd.DataFrame, y: pd.Series, sample_weights: Optional[pd.Series] = None
-    ) -> None:
-        if not hasattr(self.model, "append"):
-            return
-        use_exogenous = (
-            is_X_available(X) if self.use_exogenous is None else self.use_exogenous
-        )
-        if use_exogenous:
-            self.model = self.model.fit(starting_values=self.model.params, last_obs=y)
-        else:
-            self.model = self.model.append(endog=y, refit=True)
+        self.model = self.model.fit(disp="off")
 
     def predict(self, X: pd.DataFrame) -> Union[pd.Series, pd.DataFrame]:
         use_exogenous = (
             is_X_available(X) if self.use_exogenous is None else self.use_exogenous
         )
         if use_exogenous:
-            return pd.Series(self.model.forecast(horizon=len(X), x=X))
+            res = self.model.forecast(horizon=len(X), reindex=False, x=X)
         else:
-            return pd.Series(self.model.forecast(horizon=len(X)))
+            res = self.model.forecast(horizon=len(X), reindex=False)
+        return pd.Series(res.variance.values[0], index=X.index)
 
     def predict_in_sample(self, X: pd.DataFrame) -> Union[pd.Series, pd.DataFrame]:
         res = self.model.forecast(horizon=len(X), start=0, reindex=True)
         return res.variance[res.variance.columns[0]]
+
+    update = fit_noop
--- tests/test_arch.py
+++ tests/test_arch.py
@@ -7,21 +7,9 @@ from fold_wrappers.arch import WrapArch
 
 
 def test_arch_univariate() -> None:
-    # run_pipeline_and_check_if_results_close_univariate(
-    #     model=WrapArch(init_args=dict(vol="Garch", p=1, o=0, q=1, dist="Normal")),
-    #     splitter=ExpandingWindowSplitter(initial_train_window=50, step=1),
-    # )
-
     _, y = generate_sine_wave_data(length=200)
     y = np.log(y + 2.0).diff().dropna() * 100
-    model = WrapArch(init_args=dict(vol="Garch", p=1, o=0, q=1, dist="Normal"))
-    splitter = ExpandingWindowSplitter(initial_train_window=0.5, step=0.05)
+    model = WrapArch(init_args=dict(vol="Garch", p=1, o=1, q=1, dist="Normal"))
+    splitter = ExpandingWindowSplitter(initial_train_window=0.5, step=1)
     pred, _ = train_backtest(model, None, y, splitter)
-    assert np.isclose(y.squeeze()[pred.index], pred.squeeze().values, atol=0.1).all()
-
-
-# def test_arch_univariate_online() -> None:
-#     run_pipeline_and_check_if_results_close_univariate(
-#         model=WrapArch(init_args={"order": (1, 1, 0)}, online_mode=True),
-#         splitter=ExpandingWindowSplitter(initial_train_window=50, step=10),
-#     )
+    # assert np.isclose(y[pred.index] ** 2, pred.squeeze().values, atol=0.1).all()
